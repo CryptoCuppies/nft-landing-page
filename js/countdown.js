d5d32fa64af75f3f799b6b1f7611f1fe18e27cd2
CryptoCuppies <99137925+CryptoCuppies@users.noreply.github.com>
2022-02-06 18:58:40 +0000

    date

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 //Countdown Timer
 const clockdiv = document.getElementById("countdown");
 const countDownTime = new Date(
-  clockdiv.getAttribute("date-01/3/2022")
+  clockdiv.getAttribute("date-date")
 ).getTime();
 
 const countdownfunction = setInterval(function () {
